b4e879774c1bf4008f512d8f06e1d9b1317609cf
menard_s <simon.menard@epitech.eu>
2017-03-30 02:40:34 +0800

    testing...

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ var http                = require('http').Server(app);
 var io                  = require('socket.io')(http);
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
+    console.log("Connexion !!!!!");
+    socket.on('aaa', function(msg){
         io.emit('chat message', msg);
     });
 });
 
-http.listen(8000, function(){
+http.listen(8000, "0.0.0.0", function(){
     console.log('Sockets listen on port 8000');
 });
 //app.use("/node_modules", express.static(__dirname + '/node_modules'));
 
-/*app.use("/", express.static(__dirname + '/'));
+//app.use("/", express.static(__dirname + '/'));
+/*app.get("/", function (req, res) {
+    res.send('Hello World!');
+});
 
-app.listen(1234, function () {
-    console.log('Test on port 1234!')
+app.listen(4242, "0.0.0.0", function () {
+    console.log('Test on port 4242!')
 });*/
