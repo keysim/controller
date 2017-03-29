2f211ebf4caf796915d1c8f5129042cc06877f79
menard_s <simon.menard@epitech.eu>
2017-03-30 01:27:20 +0800

    testing...

---
+++ api/server.py
@@ -0,0 +1,33 @@
+import socket_io as io
+
+class Server(io.Server):
+    def on_connect(self, client):
+        print(client, 'connected')
+        self.broadcast(str(client) + ' connected')
+        print('there are now', len(self.clients), 'clients')
+
+    def on_message(self, client, message):
+        print(client, 'sent', message)
+        client.send(message)
+
+    def on_disconnect(self, client):
+        print(client, 'disconnected')
+        self.broadcast(str(client) + ' disconnected')
+        print('there are now', len(self.clients), 'clients')
+
+Server().listen(5000)
+
+
+#import bluetooth
+#import time
+
+bd_addr = "20:14:04:09:11:63"
+port = 1
+#sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
+#sock.connect((bd_addr,port))
+
+#while 1:
+        #time.sleep(5)
+        #sock.send("0")
+
+#sock.close()
--- server.js
+++ server.js
@@ -1,77 +1,10 @@
-var bleno = require('bleno');
+var express     = require('express');
+var app         = express();
 
+//app.use("/node_modules", express.static(__dirname + '/node_modules'));
 
-bleno.on('stateChange', function(state) {
-    console.log('State change: ' + state);
-    if (state === 'poweredOn') {
-        bleno.startAdvertising('KeyPi',['12ab']);
-    } else {
-        bleno.stopAdvertising();
-    }
-});
+app.use("/", express.static(__dirname + '/'));
 
-// Notify the console that we've accepted a connection
-bleno.on('accept', function(clientAddress) {
-    console.log("Accepted connection from address: " + clientAddress);
-});
-
-// Notify the console that we have disconnected from a client
-bleno.on('disconnect', function(clientAddress) {
-    console.log("Disconnected from address: " + clientAddress);
-});
-
-// When we begin advertising, create a new service and characteristic
-bleno.on('advertisingStart', function(error) {
-    if (error) {
-        console.log("Advertising start error:" + error);
-    } else {
-        console.log("Advertising start success");
-        bleno.setServices([
-
-            // Define a new service
-            new bleno.PrimaryService({//12ab
-                uuid : '0000110100001000800000805f9b34fb',
-                characteristics : [
-
-                    // Define a new characteristic within that service
-                    new bleno.Characteristic({//34cd
-                        value : null,
-                        uuid : '0000110100001000800000805f9b34fb',
-                        properties : ['notify', 'read', 'write'],
-
-                        // If the client subscribes, we send out a message every 1 second
-                        onSubscribe : function(maxValueSize, updateValueCallback) {
-                            console.log("Device subscribed");
-                            this.intervalId = setInterval(function() {
-                                console.log("Sending: Hi!");
-                                updateValueCallback(new Buffer("Hi!"));
-                            }, 1000);
-                        },
-
-                        // If the client unsubscribes, we stop broadcasting the message
-                        onUnsubscribe : function() {
-                            console.log("Device unsubscribed");
-                            clearInterval(this.intervalId);
-                        },
-
-                        // Send a message back to the client with the characteristic's value
-                        onReadRequest : function(offset, callback) {
-                            console.log("Read request received");
-                            callback(this.RESULT_SUCCESS, new Buffer("Echo: " +
-                                (this.value ? this.value.toString("utf-8") : "")));
-                        },
-
-                        // Accept a new value for the characterstic's value
-                        onWriteRequest : function(data, offset, withoutResponse, callback) {
-                            this.value = data;
-                            console.log('Write request: value = ' + this.value.toString("utf-8"));
-                            callback(this.RESULT_SUCCESS);
-                        }
-
-                    })
-
-                ]
-            })
-        ]);
-    }
+app.listen(1234, function () {
+    console.log('Test on port 1234!')
 });
