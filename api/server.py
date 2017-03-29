bd26cb065031cb664d3ee712c0b29a4b514326c5
menard_s <simon.menard@epitech.eu>
2017-03-30 02:14:27 +0800

    testing...

--- api/server.py
+++ api/server.py
@@ -1,18 +1,4 @@
 from socketIO_client import SocketIO
-with SocketIO('localhost', 8000) as socketIO:
+with SocketIO('192.168.1.100', 8000) as socketIO:
     socketIO.emit('aaa')
     socketIO.wait(seconds=1)
-
-#import bluetooth
-#import time
-
-bd_addr = "20:14:04:09:11:63"
-port = 1
-#sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
-#sock.connect((bd_addr,port))
-
-#while 1:
-        #time.sleep(5)
-        #sock.send("0")
-
-#sock.close()
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express     = require('express');
 var app         = express();
+var http                = require('http').Server(app);
+var io                  = require('socket.io')(http);
 
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(8000, function(){
+    console.log('Sockets listen on port 8000');
+});
 //app.use("/node_modules", express.static(__dirname + '/node_modules'));
 
-app.use("/", express.static(__dirname + '/'));
+/*app.use("/", express.static(__dirname + '/'));
 
 app.listen(1234, function () {
     console.log('Test on port 1234!')
-});
+});*/
