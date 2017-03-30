7fb357ab56286407f7092d0ecc58e78ef881de19
menard_s <simon.menard@epitech.eu>
2017-03-31 00:35:37 +0800

    test gamepad

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ print("Starting BLE...")
 
 keyduino = "20:14:04:09:11:63"
 port = 1
+size = 1024
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 print("Starting socketIO and flask...")
@@ -17,48 +18,53 @@ app = Flask(__name__, static_url_path='', template_folder='')
 
 sock.connect((keyduino, port))
 
+while 1:
+    data = sock.recv(size)
+    if data:
+        print(data)
 
-@app.route('/game/lib/<path:path>')
-def send_js(path):
-    return send_from_directory('game/lib/', path)
-
-
-@app.route('/')
-def index():
-    return render_template('game/index.html')
-
-
-@sio.on('connect', namespace='/controller')
-def connect(sid, environ):
-    print("connect ", sid)
-
-
-@sio.on('1', namespace='/controller')
-def message(sid, data):
-    sock.send("1")
-    print("message ", data)
-    # sio.emit('reply', room=sid)
-
-
-@sio.on('0', namespace='/controller')
-def message(sid, data):
-    sock.send("0")
-    print("message ", data)
-    # sio.emit('reply', room=sid)
-
-
-@sio.on('BLE', namespace='/controller')
-def connect_ble(sid, data):
-    sock.connect((keyduino, port))
-
-
-@sio.on('disconnect', namespace='/controller')
-def disconnect(sid):
-    print('disconnect ', sid)
-
-if __name__ == '__main__':
-    app = socketio.Middleware(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
+#
+# @app.route('/game/lib/<path:path>')
+# def send_js(path):
+#     return send_from_directory('game/lib/', path)
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('game/index.html')
+#
+#
+# @sio.on('connect', namespace='/controller')
+# def connect(sid, environ):
+#     print("connect ", sid)
+#
+#
+# @sio.on('1', namespace='/controller')
+# def message(sid, data):
+#     sock.send("1")
+#     print("message ", data)
+#     # sio.emit('reply', room=sid)
+#
+#
+# @sio.on('0', namespace='/controller')
+# def message(sid, data):
+#     sock.send("0")
+#     print("message ", data)
+#     # sio.emit('reply', room=sid)
+#
+#
+# @sio.on('BLE', namespace='/controller')
+# def connect_ble(sid, data):
+#     sock.connect((keyduino, port))
+#
+#
+# @sio.on('disconnect', namespace='/controller')
+# def disconnect(sid):
+#     print('disconnect ', sid)
+#
+# if __name__ == '__main__':
+#     app = socketio.Middleware(sio, app)
+#     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
 
 sock.close()
 print('Socket closed')
