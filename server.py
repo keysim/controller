33442ce28feafc5d63f61e26c4f4914fdef3ffc0
menard_s <simon.menard@epitech.eu>
2017-03-30 14:50:36 +0800

    Python server in place, updating BLE and Index.html

--- server.py
+++ server.py
@@ -1,35 +1,55 @@
 import socketio
 import eventlet
 import eventlet.wsgi
+import bluetooth
+import time
 from flask import Flask, render_template, send_from_directory
+
+keyduino = "20:14:04:09:11:63"
+port = 1
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 sio = socketio.Server()
 app = Flask(__name__, static_url_path='', template_folder='')
+sock.connect((keyduino, port))
+
 
 @app.route('/lib/<path:path>')
 def send_js(path):
     return send_from_directory('lib', path)
+
+
 @app.route('/')
 def index():
-    """Serve the client-side application."""
     return render_template('index.html')
+
 
 @sio.on('connect', namespace='/chat')
 def connect(sid, environ):
     print("connect ", sid)
 
-@sio.on('chat message', namespace='/chat')
+
+@sio.on('1', namespace='/chat')
 def message(sid, data):
+    sock.send("1")
     print("message ", data)
-    sio.emit('reply', room=sid)
+    # sio.emit('reply', room=sid)
+
+
+@sio.on('0', namespace='/chat')
+def message(sid, data):
+    sock.send("0")
+    print("message ", data)
+    # sio.emit('reply', room=sid)
+
 
 @sio.on('disconnect', namespace='/chat')
 def disconnect(sid):
     print('disconnect ', sid)
 
 if __name__ == '__main__':
-    # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
-
-    # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app)
+
+sock.close()
+print('Socket closed')
