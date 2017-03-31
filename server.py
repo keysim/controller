c9625789d6751b94dc0995ed26b6d689bbd7d05e
menard_s <simon.menard@epitech.eu>
2017-03-31 16:40:24 +0800

    test a other lib AGAIN

--- server.py
+++ server.py
@@ -92,7 +92,7 @@
 from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='game')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
